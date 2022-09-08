2b7b47715bc58abc19136ed11a5f4f700122b1b5
Michał Wesoły <mkwesoly@gmail.com>
2022-09-08 23:27:27 +0200

    moved app to GCP and DB to Postgres

--- src/main/java/com/poznan/put/student/wesoly/michal/BookSharingApp/dao/DatabaseConnection.java
+++ src/main/java/com/poznan/put/student/wesoly/michal/BookSharingApp/dao/DatabaseConnection.java
@@ -7,25 +7,21 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     public DatabaseConnection() throws SQLException {
-        Driver myDriver = new com.microsoft.sqlserver.jdbc.SQLServerDriver();
+        Driver myDriver = new org.postgresql.Driver();
         DriverManager.registerDriver( myDriver );
     }
 
-    private final static String DBURL = "jdbc:sqlserver://book-sharing-project.database.windows.net:1433;" +
-            "database=book_sharing_database;" +
-            "user=michalwesoly@book-sharing-project;" +
-            "password=d351749AB!;" +
-            "encrypt=true;" +
-            "trustServerCertificate=false;" +
-            "hostNameInCertificate=*.database.windows.net;" +
-            "loginTimeout=30;";
+    private final static String DBURL = "jdbc:postgresql://34.71.19.68:5432/postgres";
+//            "sslmode=disable" +
+//            "user=postgres;" +
+//            "password=d351749AB!;";
 
     private Connection connection = null;
     private boolean isConnected = false;
 
     public void connect() throws SQLException {
         if (!isConnected) {
-            connection = DriverManager.getConnection(DBURL);
+            connection = DriverManager.getConnection(DBURL, "postgres", "d351749AB!");
             isConnected = true;
         }
     }
